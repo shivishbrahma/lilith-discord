0f62b1940c4cd98a529d59ecad809377826f30a4
Purbayan Chowdhury <pur.cho.99@gmail.com>
2021-01-21 19:04:12 +0530

    svg render check

--- src/api/quotes.js
+++ src/api/quotes.js
@@ -2,6 +2,7 @@ const url = require('url');
 const querystring = require('querystring');
 const { Router, response } = require('express');
 const fetch = require('node-fetch');
+const { renderCard } = require('../helpers/card');
 
 const router = Router();
 
@@ -48,13 +49,7 @@ router.get('/image', (req, res) => {
 	// res.json({
 	// 	message: 'Welcome to Quotes API',
 	// });
-
-	res.send(`<svg width="400" height="180">
-	<g>
-	  <rect x="50" y="20" rx="20" ry="20" width="150" height="150"
-		style="fill:red;stroke: black;stroke-width:5;opacity:0.5"></rect>
-	</g>
-  </svg>`);
+	res.send(renderCard());
 });
 
 router.get('/', (req, res) => {
--- src/helpers/card.js
+++ src/helpers/card.js
@@ -1,7 +1,10 @@
 function renderCard() {
-	return `
-    
-    `;
+	return `<svg width="400" height="180" xmlns="http://www.w3.org/2000/svg">
+	<g>
+	  <rect x="50" y="20" rx="20" ry="20" width="150" height="150"
+		style="fill:red;stroke: black;stroke-width:5;opacity:0.5"></rect>
+	</g>
+  </svg>`;
 }
 
 module.exports = { renderCard };
